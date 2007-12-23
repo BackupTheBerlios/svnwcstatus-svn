af91f638f7fbac09d02c4ebb36182f3ed6ef71fb
twieczorek <twieczorek@a6e1a9c7-af8a-a249-9602-e031d35d26e6>
2007-12-23 01:40:44 +0000

    Release project under GPL v3 following the instructions found at http://www.fsf.org/licensing/licenses/gpl-howto.html.

--- trunk/svn_wcstatus/include/SvnFieldLoader.h
+++ trunk/svn_wcstatus/include/SvnFieldLoader.h
@@ -1,3 +1,21 @@
+/* Copyright 2007, 2008 Tom Wieczorek
+ *
+ * This file is part of svn_wcstatus.
+ *
+ * svn_wcstatus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * svn_wcstatus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with svn_wcstatus. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __TW_WDX_SVN_WCSTATUS_SVNFIELDLOADER__
 #define __TW_WDX_SVN_WCSTATUS_SVNFIELDLOADER__
 
